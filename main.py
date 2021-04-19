95b9e094a3c0c948c5e4289f3ad28673c09e0beb
Uri Berger <uriberger@yahoo.com>
2021-04-19 11:46:39 +0300

    Changed to the simplified databases (only airplane and bird)

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
-from coco import img_caption_training_set_filename, img_caption_val_set_filename
+from coco import img_caption_training_set_filename, img_caption_val_set_filename, generate_bboxes_dataset_coco
 from aux_functions import log_print, set_write_to_log
 from datetime import datetime
 from train_joint_model import train_joint_model
 import os
 from captions_dataset import ImageCaptionDataset
+from config import Config
 
 
 timestamp = str(datetime.now()).replace(' ', '_')
 os.mkdir(timestamp)
 set_write_to_log(timestamp)
 
+config = Config(image_model='resnet18')
+log_print('Main', 0, str(config))
+
 log_print('Main', 0, 'Generating datasets...')
 wanted_image_size = (224, 224)
 
-training_set = ImageCaptionDataset(wanted_image_size, img_caption_training_set_filename, 'train')
-test_set = ImageCaptionDataset(wanted_image_size, img_caption_val_set_filename, 'val')
+# training_set_filename = img_caption_training_set_filename
+# test_set_filename = img_caption_val_set_filename
+# training_set_filename = 'coco_img_caption_training_set_airplane_bird_single_class'
+# test_set_filename = 'coco_img_caption_val_set_airplane_bird_single_class'
+training_set_filename = 'coco_img_caption_training_set_airplane_bird_single_class_simplified'
+test_set_filename = 'coco_img_caption_val_set_airplane_bird_single_class_simplified'
+
+training_set = ImageCaptionDataset(wanted_image_size, training_set_filename, 'train')
+test_set = ImageCaptionDataset(wanted_image_size, test_set_filename, 'val')
 log_print('Main', 0, 'Datasets generated')
 
 log_print('Main', 0, 'Training model...')
-train_joint_model(training_set, 100, 1)
+train_joint_model(timestamp, training_set, 2, 5, config)
 log_print('Main', 0, 'Finished training model')
 
 # log_print('Main', 0, 'Testing model...')
