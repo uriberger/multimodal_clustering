3adfcee00379edaf652c85c39f4a5aaebe821792
Uri Berger <uriberger@yahoo.com>
2021-08-11 18:11:48 +0300

    Simplified the embedding model evaluator

--- executors/evaluate_embedding_model.py
+++ executors/evaluate_embedding_model.py
@@ -1,13 +1,16 @@
 import torch
 import torch.utils.data as data
+import torch.nn as nn
+import torchvision.models as models
 from utils.general_utils import generate_dataset, for_loop_with_reports
+from utils.visual_utils import generate_visual_model
 from metrics import VisualUnknownClassesClassificationMetric
 from executors.executor import Executor
 import os
-from models_src.embedding_model_wrapper import EmbeddingModelWrapper
-from models_src.clip_wrapper import ClipWrapper
 from models_src.visual_model_wrapper import VisualModelWrapper
-from models_src.model_config import ModelConfig, wanted_image_size
+from models_src.model_config import wanted_image_size
+import clip
+from sklearn.cluster import KMeans
 
 
 class EmbeddingModelEvaluator(Executor):
@@ -19,22 +22,25 @@ class EmbeddingModelEvaluator(Executor):
     model_dir = os.path.join(root_dir, 'models')
     embedding_mat_dir = os.path.join(root_dir, 'embedding_mat')
 
-    def __init__(self, test_set, class_mapping, model_name, indent):
+    def __init__(self, test_set, class_mapping, model_type, model_str, indent):
         super().__init__(indent)
 
         self.test_set = test_set
 
-        self.model = self.generate_embedding_model(model_name, len(class_mapping))
+        model, inference_func = self.generate_embedding_model(model_type, model_str)
+        self.model = model
+        self.inference_func = inference_func
         self.model.eval()
 
         self.metric = VisualUnknownClassesClassificationMetric(None)
+        model_name = model_type + '_' + model_str
         self.embedding_mat_path = os.path.join(self.embedding_mat_dir, model_name)
         if not os.path.isdir(self.embedding_mat_dir):
             os.mkdir(self.embedding_mat_dir)
 
         # Get embedding dimension
         dummy_image = torch.zeros(1, 3, wanted_image_size[0], wanted_image_size[1])
-        dummy_output = self.model.inference(dummy_image)
+        dummy_output = self.inference_func(dummy_image)
         embedding_dim = dummy_output.shape[1]
 
         self.embedding_mat = torch.zeros(len(self.test_set), embedding_dim)
@@ -43,31 +49,30 @@ class EmbeddingModelEvaluator(Executor):
         # Get gt classes num
         self.gt_classes_num = len(class_mapping)
 
-    def generate_embedding_model(self, model_name, class_num):
-        if model_name == 'pretrained_resnet':
-            model_config = ModelConfig(
-                visual_model='resnet18',
-                concept_num=class_num,
-                pretrained_visual_base_model=True
-            )
-            model = EmbeddingModelWrapper(self.device, model_config, self.model_dir, self.indent + 1, model_name)
-        elif model_name.startswith('clip_'):
-            specific_clip_model = model_name.split('clip_')[1]
-            model_config = ModelConfig(
-                visual_model=specific_clip_model
-            )
-            model = ClipWrapper(self.device, model_config, self.model_dir, self.indent + 1)
+    def generate_embedding_model(self, model_type, model_str):
+        if model_type == 'pretrained':
+            model = generate_visual_model(model_str, 1, True)
+            model.fc = nn.Identity()
+            inference_func = model.forward
+        elif model_type == 'clip':
+            model, _ = clip.load(model_str, self.device)
+            inference_func = model.encode_image
+        elif model_type == 'unimodal':
+            model_wrapper = VisualModelWrapper(self.device, None, 'models/visual', self.indent + 1, model_str)
+            model = model_wrapper.model
+            model.fc = nn.Identity()
+            inference_func = model.forward
         else:
-            model_wrapper = VisualModelWrapper(self.device, None, 'visual', self.indent + 1, model_name)
-
-        return model
+            # No such model type
+            assert False
+        return model, inference_func
 
     def evaluate(self):
         # Create embedding matrix
-        embedding_mat = generate_dataset(self.embedding_mat_path, self.run_inference_on_dataset)
+        embedding_mat = generate_dataset(self.embedding_mat_path, self.create_embedding_mat)
 
         # Cluster
-        self.model.cluster(embedding_mat)
+        self.cluster(embedding_mat)
 
         # Calculate the metric
         self.metric_on_dataset()
@@ -75,8 +80,8 @@ class EmbeddingModelEvaluator(Executor):
         # Report results
         self.report_results()
 
-    def run_inference_on_dataset(self):
-        self.log_print('Running inference on entire dataset')
+    def create_embedding_mat(self):
+        self.log_print('Creating embedding matrix')
         dataloader = data.DataLoader(self.test_set, batch_size=self.batch_size, shuffle=False)
         checkpoint_len = 10
         self.increment_indent()
@@ -98,8 +103,18 @@ class EmbeddingModelEvaluator(Executor):
             self.infer_and_record(image_tensor, batch_start, batch_end)
 
     def infer_and_record(self, image_tensor, batch_start, batch_end):
-        output = self.model.inference(image_tensor)
+        output = self.inference_func(image_tensor)
         self.embedding_mat[batch_start:batch_end, :] = output
+
+    def cluster(self, embedding_mat, soft_clustering=False):
+        if soft_clustering:
+            return
+        else:
+            kmeans = KMeans(n_clusters=self.gt_classes_num).fit(embedding_mat.detach().numpy())
+            cluster_list = list(kmeans.labels_)
+            self.image_id_to_label = {
+                x: cluster_list[x] for x in range(len(cluster_list))
+            }
 
     def metric_on_dataset(self):
         self.log_print('Running metric on entire dataset')
@@ -113,7 +128,7 @@ class EmbeddingModelEvaluator(Executor):
     def metrics_on_batch(self, index, sampled_batch, print_info):
         # Load data
         label = sampled_batch['label'].to(self.device)
-        predicted_class = self.model.image_id_to_label[index]
+        predicted_class = self.image_id_to_label[index]
         self.metric.document([[predicted_class]], [[label.item()]])
 
     def report_results(self):
