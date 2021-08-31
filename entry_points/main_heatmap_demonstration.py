088880271b81bff57ae2d859870e18297c1ecfa8
Uri Berger <uriberger@yahoo.com>
2021-08-31 12:05:38 +0300

    Fixed some issues in the entry point of heatmap demonstration

--- entry_points/main_heatmap_demonstration.py
+++ entry_points/main_heatmap_demonstration.py
@@ -16,15 +16,14 @@ function_name = 'main_heatmap_demonstration'
 os.mkdir(timestamp)
 set_write_to_log(timestamp)
 
-model_name = 'resnet_non_pretrained_noun_th_0.06_conc_num_65'
+model_name = 'resnet_50_non_pretrained_noun_th_0.03_conc_num_100'
 
 log_print(function_name, 0, 'Generating dataset_files...')
 coco_dir = os.path.join('..', 'datasets', 'COCO')
 coco = Coco(coco_dir, 1)
 
 test_set_config = DatasetConfig(1, slice_str='val', include_gt_classes=True, include_gt_bboxes=True)
-# test_set, _, _ = coco.build_dataset(test_set_config)
-test_set = coco.build_image_only_dataset(test_set_config)
+test_set, _, _ = coco.build_dataset(test_set_config)
 class_mapping = coco.get_class_mapping()
 log_print(function_name, 0, 'Datasets generated')
 
