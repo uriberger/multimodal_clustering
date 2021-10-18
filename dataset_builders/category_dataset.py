cd02c2fc4a9e57ca25571bda225372273133f4fd
Uri Berger Laptop <uriberger@yahoo.com>
2021-10-18 22:31:14 +0300

    Added fountain's dataset to the categorization metric

--- dataset_builders/category_dataset.py
+++ dataset_builders/category_dataset.py
@@ -1,12 +1,15 @@
 from utils.general_utils import generate_dataset
 import os
+import yaml
 
 cache_dirname = 'cached_dataset_files'
 category_dataset_filename = os.path.join(cache_dirname, 'category_dataset')
 extended_category_dataset_filename_prefix = os.path.join(cache_dirname, 'extended_category_dataset_')
+fountain_dataset_filename = 'fountain_dataset'
 category_filename = 'multilingual_concepts_categories.tsv'
 extended_category_filename_prefix = 'category_dataset_'
 extended_category_filename_suffix = '.txt'
+fountain_filename = 'mcrae_typicality.yaml'
 
 
 def generate_category_dataset():
@@ -67,3 +70,25 @@ def generate_extended_category_dataset_internal(dataset_filename):
                 category_to_words[cur_category].append(token)
 
     return category_to_words
+
+
+def generate_fountain_category_dataset():
+    return generate_dataset(fountain_dataset_filename, generate_fountain_category_dataset_internal, fountain_filename)
+
+
+def generate_fountain_category_dataset_internal(dataset_filename):
+    print('Generating category dataset...')
+
+    with open(dataset_filename) as f:
+        base_category_to_word = yaml.load(f, Loader=yaml.FullLoader)
+        word_lists = [list(x.keys()) for x in base_category_to_word.values()]
+        all_words = list(set([word for outer in word_lists for word in outer]))
+        word_to_categories = {word: {
+            x[0]: x[1][word] for x in base_category_to_word.items() if word in x[1]
+        } for word in all_words}
+        word_to_category = {x[0]: max(x[1], key=x[1].get) for x in word_to_categories.items()}
+        all_categories = list(word_to_category.values())
+        category_to_word_list = {categ: [x[0] for x in word_to_category.items() if x[1] == categ]
+                                 for categ in all_categories}
+
+        return category_to_word_list
