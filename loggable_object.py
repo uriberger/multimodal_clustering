3f7e9d24a19985d0e2ab12db983b22e5c9db56c0
Uri Berger Laptop <uriberger@yahoo.com>
2021-12-03 17:39:29 +0200

    Added comments to loggable_object file

--- loggable_object.py
+++ loggable_object.py
@@ -1,8 +1,19 @@
+###############################################
+### Unsupervised Multimodal Word Clustering ###
+### as a First Step of Language Acquisition ###
+###############################################
+# Written by Uri Berger, December 2021.
+#
+# COMMERCIAL USE AND DISTRIBUTION OF THIS CODE, AND ITS MODIFICATIONS,
+# ARE PERMITTED ONLY UNDER A COMMERCIAL LICENSE FROM THE AUTHOR'S EMPLOYER.
+
 from utils.general_utils import log_print
 
 
 class LoggableObject:
-    """ The top class from which all other classes inherit. """
+    """ The top class from which all other classes inherit.
+    Enables printing to the global log (using the log_print) function, with indent. The indent is stored in the
+    instance, and can be incremented or decremented. """
 
     def __init__(self, indent):
         self.indent = indent
