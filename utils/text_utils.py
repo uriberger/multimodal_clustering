52919d82b82db34e1810852b4a014fe7a919d88f
Uri Berger Laptop <uriberger@yahoo.com>
2021-12-15 18:43:33 +0200

    Improved the text_utils.py file

--- utils/text_utils.py
+++ utils/text_utils.py
@@ -1,5 +1,17 @@
+###############################################
+### Unsupervised Multimodal Word Clustering ###
+### as a First Step of Language Acquisition ###
+###############################################
+# Written by Uri Berger, December 2021.
+#
+# COMMERCIAL USE AND DISTRIBUTION OF THIS CODE, AND ITS MODIFICATIONS,
+# ARE PERMITTED ONLY UNDER A COMMERCIAL LICENSE FROM THE AUTHOR'S EMPLOYER.
+
 import torch.nn as nn
 import spacy
+
+""" This file contains functions and definitions related to text used across all the project. """
+
 nlp = spacy.load('en_core_web_sm')
 tokenizer = nlp.tokenizer
 
@@ -40,39 +52,18 @@ def preprocess_token(token):
 
 
 def prepare_data(captions, lemmatize=False):
+    """ Tokenize and clean a list of input captions. """
     token_lists = []
     for caption in captions:
-        # token_list = caption.split()
-        # token_list = [preprocess_token(token) for token in token_list]
-        # token_list = [x for x in token_list if len(x) > 0]
         if lemmatize:
             doc = nlp(caption)
             token_list = [str(x.lemma_) for x in doc]
         else:
-            # token_list = [str(x) for x in doc]
             token_list = [str(x) for x in list(tokenizer(caption.lower()))]
         token_lists.append(token_list)
     return token_lists
 
 
 def multiple_word_string(my_str):
+    """ Check if the input string has multiple words. """
     return len(my_str.split()) > 1
-
-
-def generate_text_model(model_str, output_size, word_embed_dim):
-    # We don't know what the size of the vocabulary will be, so let's take some large value
-    vocab_size = 50000
-    num_layers = 2
-
-    if model_str == 'lstm':
-        model = nn.Sequential(
-            nn.Embedding(vocab_size, word_embed_dim),
-            nn.LSTM(word_embed_dim, output_size, num_layers, batch_first=True)
-        )
-    elif model_str == 'gru':
-        model = nn.Sequential(
-            nn.Embedding(vocab_size, word_embed_dim),
-            nn.GRU(word_embed_dim, output_size, num_layers, batch_first=True)
-        )
-
-    return model
